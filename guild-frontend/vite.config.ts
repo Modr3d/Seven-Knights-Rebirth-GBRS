0fce7f430c54aaf51cbfc16462245fd0147beecf
Burit S <burit3594@gmail.com>
2025-11-17 16:19:43 +0700

    fix

--- guild-frontend/vite.config.ts
+++ guild-frontend/vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import viteStaticCopy from 'vite-plugin-static-copy'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
@@ -8,14 +7,6 @@ export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
-    viteStaticCopy({
-      targets: [
-        {
-          src: '_redirects',
-          dest: '',
-        },
-      ],
-    }),
   ],
   base: '/',
   build: {
